572a6ca2441fac95dd4b2b93d24cc48b2f76441c
lidiofidelis <lidiojunior44@gmail.com>
2021-11-04 03:43:12 -0300

    Version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+// the number of the LED pin
+const int ledPin = 2;  // 2 corresponds to GPIO2
 
-void setup() {
-  // put your setup code here, to run once:
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 16;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
+ 
+void loop(){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(20);
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(20);
+  }
 }
